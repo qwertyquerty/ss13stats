b43fed5553e699b792eb1ecc10d3a656ae1286fb
qwerty <qwertytrogi@gmail.com>
2022-07-30 20:11:33 -0500

    add port to config

--- run_server.py
+++
@@ -1,4 +0,0 @@
-from ss13stats import flask
-
-if __name__ == "__main__":
-	flask.run(debug=True)
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from ss13stats import flask
+from ss13stats.config import cfg
+
+if __name__ == "__main__":
+	flask.run(debug=True, port=cfg.get("port"))
