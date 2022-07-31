0f64541ead47cb2b0a775407dfb77804d518a1f2
qwerty <qwertytrogi@gmail.com>
2022-07-30 20:19:48 -0500

    fix port

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ from ss13stats import flask
 from ss13stats.config import cfg
 
 if __name__ == "__main__":
-	flask.run(debug=True, port=cfg.get("port"))
+	flask.run(debug=True, port=cfg.get("website.port"))
